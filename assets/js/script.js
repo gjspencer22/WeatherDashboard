9e1763144f5554d390f80a93e8b35dbb984b791a
gjspencer22 <gjspencer22@gmail.com>
2021-06-15 19:42:10 -0600

    working city buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ function find() {
         findCity.value + 
         '&appid=7938b2c7d262537079eede933a87d70c')
         .then(response => response.json())
-        .then(data => createButton(data))
+        .then(data => showCity(data))
         // .catch(function (error) {
         //     alert
             
@@ -25,10 +25,7 @@ function cityButtonClick(event) {
             event.target.textContent +
             '&appid=7938b2c7d262537079eede933a87d70c')
             .then(response => response.json())
-            .then(data => currentCity(data.name))
-            console.log(currentCity)
-            // var newDisp = currentCity(data.name)
-            // displayCity.innerHTMl = newDisp
+            .then(data => showCity(data))
             // .catch(function (error) {
             //     alert
             // })
@@ -37,40 +34,23 @@ function cityButtonClick(event) {
     }
 
 }
-function createButton(data) {
-    // console.log(data.name)
-    var newButton = document.createElement('button');
-    newButton.innerText = data.name
-    cityListEl.append(newButton)
-    displayCity.innerHTML = currentCity(data.name, data.main.temp)
-}
+// function createButton(data) {
+//     // console.log(data.name)
+//     var newButton = document.createElement('button');
+//     newButton.innerText = data.name
+//     cityListEl.append(newButton)
+//     displayCity.innerHTML = currentCity(data.name, data.main.temp)
+// }
 
-function displayCity(data) {
-
-    console.log(data);
-    // var displayCity = document.getElementById('currentCityCountry');
-    
-
-
-    var temp = data.main.temp - 273.15
+function showCity(data) {
     var name = data.name
+    var temp = ((((data.main.temp -273.15 ) *9) /5)+32).toFixed(2)
     var weather = data.weather[0].description
     var country = data.sys.country
-    displayCity.innerHTML = currentCity(name, temp);
-    //     displayCity.innerText = temp
-    //     displayCity.innerText = weather
-    //     displayCity.innerText = country
+    displayCity.innerHTML = currentCity(name, temp, weather,country);
 }
-function currentCity(name, temp) {
-    var template = `<div><p>${name}</p><p>${temp}</p></div>`
+function currentCity(name, temp, weather,country) {
+    var template = `<div><p>${name}</p><p>${temp}&deg; F</p><p>The skies are ${weather}</p><p>${country}</p></div>`
     return template;
 }
 
-
-
-
-var getFeaturedRepos = function(language) {
-    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
-  
-    fetch(apiUrl);
-  };
